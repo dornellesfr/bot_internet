2327eb09e9bcfe083b56f5ec4e977c579bd5dd74
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2022-03-23 15:29:22 -0300

    complet

--- bot-login-hotmail.py
+++ bot-login-hotmail.py
@@ -11,6 +11,10 @@ web.get('https://login.live.com/')
 web.find_element(By.XPATH, '//*[@id="i0116"]').send_keys('automacao_teste@hotmail.com')
 web.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
 web.find_element(By.XPATH, '//*[@id="i0118"]').send_keys('senha123')
-sleep(1)
+sleep(2)
 web.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
-
+if web.find_element(By.XPATH, '//*[@id="lightbox"]/div[3]/div/div[2]/div/div[1]'):
+    sleep(2)
+    web.find_element(By.XPATH, '//*[@id="idBtn_Back"]').click()
+sleep(2)
+web.close()
